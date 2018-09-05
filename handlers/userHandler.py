bd60740e72a001e78a8656709db1e767407d69cc
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-09-05 00:00:25 -0300

    getting all the users

--- handlers/userHandler.py
+++ handlers/userHandler.py
@@ -5,10 +5,14 @@ from flask import jsonify, Blueprint, request
 
 user = Blueprint('user', __name__)
 
+current_directory = os.getcwd()
+DB_PATH = f'{current_directory}/db/db_examen.db'
+
 @user.route('/user', methods=['POST', 'GET'])
-def create():
+def usuarios():
   if request.method == 'POST':
     user = request.get_json()
+
     id = user['user']['id']
     rut = user['user']['rut']
     dv = user['user']['dv']
@@ -28,4 +32,26 @@ def create():
     renta = user['user']['renta']
     sueldo_liquido = user['user']['sueldo_liquido']
     enfermedad_cronica = user['user']['enfermedad_cronica']
-    return jsonify({ 'msg': sueldo_liquido})
+
+    conn = sqlite3.connect(DB_PATH)
+
+    query = f'INSERT INTO usuario (id, rut, dv, nombre, apellido_paterno, apellido_materno, fecha_nacimiento, password, sexo, estado_civil, hijos, telefono, email, direccion, comuna, educacion, renta, sueldo_liquido, enfermedad_cronica) values ("{id}", "{rut}", "{dv}", "{nombre}", "{apellido_paterno}", "{apellido_materno}", "{fecha_nacimiento}", "{password}", "{sexo}", "{estado_civil}", "{hijos}", "{telefono}", "{email}", "{direccion}", "{comuna}", "{educacion}", "{renta}", "{sueldo_liquido}", "{enfermedad_cronica}")'
+
+    cursor = conn.execute(query)
+
+    conn.commit()
+
+    return jsonify({ 'msg': cursor.lastrowid})
+
+
+  if request.method == 'GET':
+
+    print('get method')
+    
+    conn = sqlite3.connect(DB_PATH)
+
+    cursor = conn.execute('SELECT * FROM usuario')
+
+    usuarios = cursor.fetchall()
+
+    return jsonify({'msg': usuarios})
