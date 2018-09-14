bb74e4f9e444e4dfc7c602a0c7f51324fa4c1644
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-09-14 12:02:59 -0300

    after zip

--- handlers/creditHandler.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify, Blueprint
-
-credit = Blueprint('credit', __name__)
-
-@credit.route('/')
-def get():
-  return jsonify({ 'msng': 'get method credit'})
--- handlers/testHandler.py
+++ handlers/testHandler.py
@@ -5,5 +5,4 @@ testBlueprint = Blueprint('testing', __name__)
 @testBlueprint.route('/test', methods=['GET'])
 def requestTest():
     if request.method == 'GET':
-        print('WEAAA')
         return jsonify({ 'msg': 'testing', 'status': 200})
